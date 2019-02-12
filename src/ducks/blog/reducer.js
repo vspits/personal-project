921f68cd94dd0072dc0084e8dc774917af0b6477
vivian <vivian@vivians-MacBook-Pro.local>
2019-02-11 21:00:52 -0700

    added skeleton redux structure

---
+++ src/ducks/blog/reducer.js
@@ -0,0 +1,24 @@
+
+
+const initialState = {
+    
+}
+
+
+// // // CONSTANTS // // //
+
+
+
+// // // ACTION BUILDERS // // //
+
+
+
+// // // REDUCER FUNCTION // // //
+
+export default function reducer(state = initialState, action){
+    const { type, payload } = action
+    switch(type){
+        default:
+            return state
+    }
+}
